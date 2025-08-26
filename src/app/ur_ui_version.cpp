daaea9a5a2e72751b40260fb8ec70c194b6eab3a
Zoheb Shujauddin <zoheb2424@gmail.com>
2025-08-26 15:46:16 +0530

    adds new tutorial images, version text; updates credits text

---
+++ src/app/ur_ui_version.cpp
@@ -0,0 +1,27 @@
+#include <string>
+
+#include "ui_text.hpp"
+#include "ur_ui_version.hpp"
+
+#include "version.h"
+
+
+std::shared_ptr<ToyMakersEngine::BaseSimObjectAspect> UrUIVersion::create(const nlohmann::json& jsonAspectProperties) {
+    (void)jsonAspectProperties; // prevent unused parameter warnings
+    return std::shared_ptr<UrUIVersion>{ new UrUIVersion{} };
+}
+
+std::shared_ptr<ToyMakersEngine::BaseSimObjectAspect> UrUIVersion::clone() const {
+    return std::shared_ptr<UrUIVersion>{ new UrUIVersion{} };
+}
+
+void UrUIVersion::onActivated() {
+    getAspect<UIText>().updateText(
+        "Game Of Ur v" 
+        + std::to_string(Game_Of_Ur_VERSION_MAJOR) 
+        + "." 
+        + std::to_string(Game_Of_Ur_VERSION_MINOR)
+        + "."
+        + std::to_string(Game_Of_Ur_VERSION_PATCH)
+    );
+}
